654be97bc601d7ba6f889b08be3dfee2d751140e
Carowo-design <miftarulhoque@web.de>
2026-03-12 11:56:09 +0100

    Initial commit

---
+++ lightbox.js
@@ -0,0 +1,8 @@
+  function openLightbox(img) {
+    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('lightbox-img').src = img.src;
+  }
+
+  function closeLightbox() {
+    document.getElementById('overlay').style.display = 'none';
+  }
